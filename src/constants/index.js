0dcbf3b8b60f1f48507669aa29e67cfa0ec9d94d
Houngue Yaovi <hfyaovi@gmail.com>
2020-06-01 23:27:19 +0300

    fix cors issues

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const IPIFY_URL = 'https://api.ipify.org';
+export const HEROKU_CORS_URL = 'https://cors-anywhere.herokuapp.com';
 export const IP_STACK_URL = 'http://api.ipstack.com/';
 export const IP_STACK_KEY = process.env.REACT_APP_IP_STACK_KEY;
 export const IP_STACK_PARAMS = `?access_key=${IP_STACK_KEY}`;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
-import { IPIFY_URL, IP_STACK_URL, IP_STACK_PARAMS } from '../constants';
+import {
+  IPIFY_URL,
+  IP_STACK_URL,
+  IP_STACK_PARAMS,
+  HEROKU_CORS_URL,
+} from '../constants';
 
 export const getCity = async () => {
   try {
-    const ip = await (await axios.get(IPIFY_URL)).data;
-    return await axios.get(`${IP_STACK_URL}${ip}${IP_STACK_PARAMS}`);
+    const ip = await (await axios.get(`${HEROKU_CORS_URL}/${IPIFY_URL}`)).data;
+    console.log(ip);
+    return await axios.get(
+      `${HEROKU_CORS_URL}/${IP_STACK_URL}${ip}${IP_STACK_PARAMS}`
+    );
   } catch (error) {
     return error;
   }
 };
 
-export const fetchData = async url => {
+export const fetchData = async (url) => {
   try {
     return await axios.get(url);
   } catch (error) {
